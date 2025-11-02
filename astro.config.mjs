8e20ac6207bb527efd620c4d025f3a217523dfb7
Unaic <unaicc2003@gmail.com>
2025-11-02 19:44:44 +0100

    feat: Configurar salida estática en Astro y añadir archivo de configuración para Render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static'
 });
